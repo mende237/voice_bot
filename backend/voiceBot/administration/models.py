5aa03d54545d6a00e85d263d625568fc026b4c10
MbeHenri <henri.mbe@facsciences-uy1.cm>
2022-05-08 12:39:47 +0100

    format de formulation 1

--- backend/voiceBot/administration/models.py
+++ backend/voiceBot/administration/models.py
@@ -19,49 +19,21 @@ class Feuille(Noeud):
     description = models.TextField(max_length=500, blank=True)
     CHEMIN_FILE_FORMAT_FORMULATION = ""
     
-    def generation_format_Formulation(self):
-        pass
-        
-    def genration_Formulaire(self):
-        attributs = self.get_caracteristique()
-        ch = ""
-        for att in attributs :
-            ch = ch + att.get_format_sur_formulaire()
-        return ch
-        
+     
+class formulation(models.Model):
+    format_formulation = models.CharField(max_length=400, blank=True)
+    
+    pass
     
 class Caracteristique(models.Model):
     nom = models.CharField(max_length=50)
     type = models.CharField(max_length=2, choices = TYPE_CHOICES)
     feuille = models.ForeignKey(Feuille, on_delete=models.CASCADE)
     
-    def get_format_sur_formulaire(self):
-        ch = "< input "
-        pass
         
 class Administrateur(User):
     image = models.ImageField(upload_to="uploads/user/admin/", blank=True)
     
-    def ajouter_un_noued(self,nom):
-        if self.is_authenticated :
-            return True
-        else :
-            return False
-        
-    def supprimer_un_noeud(noeud):
-        pass
-        
-    def definir_un_noeud_comme_feuille(noeud):
-        pass
-    
-    def format_formulation(feuille, chaine, list_caracteristiques ):
-        pass
-        
-    def ajouter_caracteristique(feuille, caracteristique ):
-        pass
-        
-    def ajouter_ensignant(enseignant):
-        pass
     
     
 
--- backend/voiceBot/administration/urls.py
+++ backend/voiceBot/administration/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("create_leaf", views.create_leaf,name='create_leaf'),
     path("form_leaf", views.form_leaf,name='form_leaf'),
     path("delete_noeud", views.delete_noeud,name='delete_noeud'),
+    path("form_format_formulation", views.form_format_formulation,name='form_format_formulation'),
+    path("add_format_formulation", views.add_format_formulation,name='add_format_formulation'),
     path("home", views.home, name="Home"),
 ]
