846d3b4720dad873ef6e3955289e9f0435356c5b
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2022-05-23 12:13:43 +0100

    test commit

--- backend/voiceBot/voiceBot/settings.py
+++ backend/voiceBot/voiceBot/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'authentification',
 
 ]
-
+#herman
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
